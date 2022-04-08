4a1416d5514e68ca84fe960cc12a173f1bedbfbe
Mike Baldini <mbaldini@live.com>
2022-04-07 19:32:37 -0700

    simple update

--- taillights.ino
+++ taillights.ino
@@ -1,26 +1,91 @@
 #include <FastLED.h>
 
-#define LED_PIN     7
-#define NUM_LEDS    256
-#define NUM_ROWS    8
-#define NUM_COLUMNS 32
+#define LED_PIN             7
+#define NUM_LEDS            256
+#define NUM_ROWS            8
+#define NUM_COLUMNS         32
+#define RUNNING_LIGHTS_PIN  8
+#define LEFT_TURN_PIN       11
+#define RIGHT_TURN_PIN      12
 
 CRGB leds[NUM_LEDS];
 int leftColIndex = 16;
 int rightColIndex = 16;
 int stopColIndex = 16;
 int numOn = 8;
+int runningLightBrightness = 50;
+int fullBrightness = 255;
+bool wasTurningRight = false;
+bool wasTurningLeft = false;
+bool wasStopping = false;
+bool wasRunningLights = false;
+int loopCounter = 0;
+
 
 void setup() {
+  pinMode(RUNNING_LIGHTS_PIN, INPUT);
+  pinMode(LEFT_TURN_PIN, INPUT);
+  pinMode(RIGHT_TURN_PIN, INPUT);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  
   for (int x = 0; x < NUM_COLUMNS; x++) {
     illuminateColumn(x, 0, 0, 0);
   }
+
   FastLED.show();
+
+  Serial.begin(9600);
+  Serial.println("Arduino is ready");
 }
 
 void loop() {
-  stop();
+  // read pins every 5 loops
+  bool isTurningLeft = wasTurningLeft;
+  bool isTurningRight = wasTurningRight;
+  bool isStopping = wasStopping;
+  bool isRunningLights = wasRunningLights;
+
+  if (loopCounter++ == 10) {
+    int runningLights = digitalRead(RUNNING_LIGHTS_PIN);
+    int left = digitalRead(LEFT_TURN_PIN);
+    int right = digitalRead(RIGHT_TURN_PIN);
+  
+    loopCounter = 0;
+    isTurningLeft = left == 1 && right != 1;
+    isTurningRight = left == 0 && right == 1;
+    isStopping = left == 1 && right == 1;
+    isRunningLights = runningLights == 1;
+
+    if (isRunningLights != wasRunningLights) {
+      Serial.print("RunningLights: ");
+      Serial.println(isRunningLights);
+      wasRunningLights = isRunningLights;
+    }
+
+    if (wasStopping != isStopping) {
+      Serial.print("Stopping: ");
+      Serial.println(isStopping);
+      wasStopping = isStopping;
+    }
+    if (isTurningLeft != wasTurningLeft) {
+      Serial.print("Turning Left: ");
+      Serial.println(isTurningLeft);
+      wasTurningLeft = isTurningLeft;
+    }
+    if (isTurningRight != wasTurningRight) {
+      Serial.print("Turning Right: ");
+      Serial.println(isTurningRight);
+      wasTurningRight = isTurningRight;
+    }
+  }
+
+  if (isStopping) {
+    stop();
+  } else if (isTurningLeft) {
+    turnLeft();
+  } else if (isTurningRight) {
+    turnRight();
+  }
   delay(30);
 }
 
@@ -43,11 +108,12 @@ void illuminateColumn(int x, int r, int g, int b) {
 
 void turnLeft() {
   // right to left
+  int offBrightness = wasRunningLights ? runningLightBrightness : 0;
   for (int x = -8; x < (NUM_COLUMNS / 2) + 8; x++) {
     if (x < 0 || x >= NUM_COLUMNS / 2) continue;
     // is column on?
     bool isOn = leftColIndex >= x && leftColIndex <= x + numOn;
-    illuminateColumn(x, isOn ? 255 : 0, 0, 0);
+    illuminateColumn(x, isOn ? fullBrightness : offBrightness, 0, 0);
   }
   FastLED.show();
   if (leftColIndex-- < -8){
@@ -57,11 +123,12 @@ void turnLeft() {
 
 void turnRight() {
   // left to right
+  int offBrightness = wasRunningLights ? runningLightBrightness : 0;
   for (int x = (NUM_COLUMNS / 2) - 8; x < NUM_COLUMNS + 8; x++) {
     if (x < NUM_COLUMNS / 2 || x > NUM_COLUMNS) continue;
     // is column on?
     bool isOn = rightColIndex <= x && rightColIndex >= x - numOn;
-    illuminateColumn(x, isOn ? 255 : 0, 0, 0);
+    illuminateColumn(x, isOn ? fullBrightness : offBrightness, 0, 0);
   }
   FastLED.show();
   if (rightColIndex++ > NUM_COLUMNS + 8){
@@ -71,11 +138,12 @@ void turnRight() {
 
 void stop() {
   // right to left
+  int offBrightness = wasRunningLights ? runningLightBrightness : 0;
   for (int x = 15; x >= 0; x--) {
     // is column on?
-    bool isOn = stopColIndex < (NUM_COLUMNS / 2) - x;
-    illuminateColumn(x + 16, isOn ? 255 : 0, 0, 0);
-    illuminateColumn(16 - x, isOn ? 255 : 0, 0, 0);
+    bool isOn = stopColIndex <= (NUM_COLUMNS / 2) - x;
+    illuminateColumn(x + 16, isOn ? fullBrightness : offBrightness, 0, 0);
+    illuminateColumn(16 - x, isOn ? fullBrightness : offBrightness, 0, 0);
   }
   FastLED.show();
   if (stopColIndex > 0){
